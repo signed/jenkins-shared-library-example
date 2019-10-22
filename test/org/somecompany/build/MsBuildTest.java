a4e0e0445b2950e095462e16dba7481e502e9c9f
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-10-22 21:49:54 +0200

    use junit 5

--- test/org/somecompany/build/MsBuildTest.java
+++ test/org/somecompany/build/MsBuildTest.java
@@ -1,18 +1,15 @@
 package org.somecompany.build;
 
+import org.junit.jupiter.api.Test;
 import org.somecompany.StepExecutor;
-import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
-/**
- * Example test class
- */
-public class MsBuildTest {
+class MsBuildTest {
     private StepExecutor steps = mock(StepExecutor.class);
 
     @Test
-    public void build_callsShStep() {
+    void build_callsShStep() {
         String solutionPath = "some/path/to.sln";
         MsBuild builder = new MsBuild(steps, solutionPath, true);
 
@@ -22,7 +19,7 @@ public class MsBuildTest {
     }
 
     @Test
-    public void build_shStepReturnsStatusNotEqualsZero_callsErrorStep() {
+    void build_shStepReturnsStatusNotEqualsZero_callsErrorStep() {
         String solutionPath = "some/path/to.sln";
         MsBuild builder = new MsBuild(steps, solutionPath, true);
 
