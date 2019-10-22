f194ccb15a817b6e3699e0f642c8c770346717af
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2019-10-22 21:19:59 +0200

    do not use the singleton

--- src/org/somecompany/build/MsBuild.groovy
+++ src/org/somecompany/build/MsBuild.groovy
@@ -1,23 +1,20 @@
 package org.somecompany.build
 
 import org.somecompany.IStepExecutor
-import org.somecompany.ioc.ContextRegistry
 
 /**
  * Example class (without proper implementation) for using the MsBuild tool for building .NET projects.
  */
 class MsBuild implements Serializable {
-    private final IStepExecutor stepExecutor
+    private final IStepExecutor steps
     private final String solutionPath
 
-    MsBuild(String solutionPath, IStepExecutor stepExecutor) {
-        this.stepExecutor = stepExecutor
+    MsBuild(String solutionPath, IStepExecutor steps) {
+        this.steps = steps
         this.solutionPath = solutionPath
     }
 
     void build() {
-        IStepExecutor steps = ContextRegistry.getContext().getStepExecutor()
-
         int returnStatus = steps.sh("echo \"building ${this.solutionPath}...\"")
         if (returnStatus != 0) {
             steps.error("Some error")
